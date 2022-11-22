9dcf9d19ffe4b21bcb14bc30e7d1f9af551a1c3e
Asger Svenning <asgersvenning@gmail.com>
2022-11-22 13:00:40 +0100

    do slicing using multiprocessing in 'finalizeDataset.py' using p_tqdm.p_map

--- slicing/finalizeDataset.py
+++ slicing/finalizeDataset.py
@@ -1,5 +1,4 @@
 import glob
-import multiprocessing.dummy as mpd
 import os
 import re
 import sys
@@ -7,7 +6,7 @@ from shutil import rmtree
 
 from data_cleaning import create_yolo_annotations
 from image_resizing import resize_images
-from tqdm import tqdm
+from p_tqdm import p_map
 
 from slicing import slice_image
 
@@ -22,7 +21,30 @@ def read_directories(path : str) -> Tuple[str, str, str]:
 
     return tuple(lines)
 
-def main(downscaling_factor : str = "4", split : str = "0.8,0.16,0.04", num_subset : str = None, verbose : str = "False") -> None:
+def tile_one_image(reduced_file: str, sliced_directory: str) -> None:
+        image = re.sub("[/\\\\]+", "/", reduced_file)
+        annotation = re.sub("(?<=/)images(?=/)", "labels", image)
+        annotation = re.sub("\.[a-zA-Z]+$", ".txt", annotation)
+        
+        image_uuid = re.search("[a-zA-Z0-9_]+(?=\.txt$)", annotation).group(0) # Global unique image identifier
+        uuid_parts = image_uuid.rsplit("_")
+        series = "_".join(uuid_parts[:2]) # Image series
+        local_id = "_".join(uuid_parts[2:]) # Series unique image identifier
+
+        slice_image(
+            image=image,
+            yolo_annotation=annotation,
+            output_dir=f'{sliced_directory}{os.sep}*{os.sep}{series}{os.sep}{local_id}', 
+            output_file_name=image_uuid + "_bbox",
+            slice_height=640,
+            slice_width=640,
+            overlap_height_ratio=0.1,
+            overlap_width_ratio=0.1,
+            min_out_slice_annotations=1,
+            verbose=False
+        )
+
+def main(downscaling_factor : str = "4", num_subset : str = None, verbose : str = "False", workers : str = "8") -> None:
 
     if not num_subset is None:
         try:
@@ -37,17 +59,14 @@ def main(downscaling_factor : str = "4", split : str = "0.8,0.16,0.04", num_subs
         raise ValueError("Argument verbose must be one of either 'True' or 'False'")
 
     try:
-        split = tuple([float(i) for i in split.rsplit(",")])
-        print(split)
-        if not len(split) == 3 or not sum(split) == 1:
-            raise ValueError("Argument 'split' must contain 3 values that add to 1.")
-    except:
-        raise ValueError("Argument 'split' must be a string consisting of 3 numbers separated by commas and adding to 1.")
-
-    try:
         downscaling_factor = int(downscaling_factor)
     except:
         raise ValueError("dummy")
+    
+    try:
+        workers = int(workers)
+    except:
+        raise ValueError("Argument 'workers' must be an integer.")
     
     source_directory, reduced_directory, sliced_directory = read_directories("directories.txt") 
 
@@ -75,7 +94,7 @@ def main(downscaling_factor : str = "4", split : str = "0.8,0.16,0.04", num_subs
 
     image_path_pattern = f'{reduced_directory}{os.sep}images{os.sep}**{os.sep}**.jpg'
     sliced_image_path_pattern = f'{sliced_directory}{os.sep}images{os.sep}**.jpg'
-    annotation_path_pattern = f'{reduced_directory}{os.sep}labels{os.sep}**{os.sep}**.txt'
+    # annotation_path_pattern = f'{reduced_directory}{os.sep}labels{os.sep}**{os.sep}**.txt' # Not used currently
 
     origin_image_set = {re.search("[a-zA-Z0-9_]+(?=\.[a-zA-Z]{2,4}$)", i).group(0) for i in glob.iglob(image_path_pattern)}
     slice_image_set = {re.search("[a-zA-Z0-9_]+(?=(_[0-9]{1,4}){4}\.[a-zA-Z]{2,4}$)", i).group(0) for i in glob.iglob(sliced_image_path_pattern)}
@@ -91,39 +110,10 @@ def main(downscaling_factor : str = "4", split : str = "0.8,0.16,0.04", num_subs
         if len(possible_left) == 0:
             continue
         left_image_paths.append(possible_left[0])
-
-    with tqdm(left_image_paths) as t:
-
-        def tile_one_image(reduced_file: str) -> None:
-            t.set_description("Tiling " + reduced_file)
-
-            image = re.sub("[/\\\\]+", "/", reduced_file)
-            annotation = re.sub("(?<=/)images(?=/)", "labels", image)
-            annotation = re.sub("\.[a-zA-Z]+$", ".txt", annotation)
-            
-            image_uuid = re.search("[a-zA-Z0-9_]+(?=\.txt$)", annotation).group(0) # Global unique image identifier
-            uuid_parts = image_uuid.rsplit("_")
-            series = "_".join(uuid_parts[:2]) # Image series
-            local_id = "_".join(uuid_parts[2:]) # Series unique image identifier
-
-            slice_image(
-                image=image,
-                yolo_annotation=annotation,
-                output_dir=f'{sliced_directory}{os.sep}*{os.sep}{series}{os.sep}{local_id}', # + re.search("(?<=/images)/[a-zA-Z0-9_-]+(?=/)", image).group(0),
-                output_file_name=image_uuid + "_bbox",
-                slice_height=640,
-                slice_width=640,
-                overlap_height_ratio=0.1,
-                overlap_width_ratio=0.1,
-                min_out_slice_annotations=1,
-                verbose=False
-            )
         
-        for i in t:
-            tile_one_image(i)
+    p_map(tile_one_image, left_image_paths, [sliced_directory for i in left_image_paths])
 
 if __name__ == '__main__':
-    # raise NotImplementedError("Train/Validation/Test splitting not implemented!")
     kwargs = {}
     for i in sys.argv[1:]:
         v = re.search("(?<==).+$", i).group(0)
